cc57de9454127c5c1003cf622581d0b086f2f3a6
roboticstechnology <roboticstechnology@gmail.com>
2021-11-15 19:16:37 +0300

    feat: add validate path to i/o files

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const log = console.log;
+import fs from 'fs';
+import path from 'path';
+import util from 'util';
+const fsAccess = util.promisify(fs.access);
+
+
 
 class CustomExceptionValidate extends Error {
 
@@ -9,41 +15,71 @@ class CustomExceptionValidate extends Error {
     }
 }
 
-try {
+class CustomExceptionExistsFile extends Error {
 
-    log(process.argv)
-    const CLIoptions = {};
-    process.argv.forEach((element, ind) => {
+    constructor(message, path, _name = 'NoExistsFile') {
+        super(message);
+        this.path = path;
+        this.name = _name;
 
-        if (element === '-c' || element === '--config') {
-
-            if (CLIoptions.hasOwnProperty('-c')) throw new CustomExceptionValidate('duplicated option -c', 'ValidateDublicatedOptionsError');
-            CLIoptions['-c'] = process.argv[ind + 1];
-
-        }
-
-        if (element === '-i' || element === '--input') {
-
-            if (CLIoptions.hasOwnProperty('-i')) throw new CustomExceptionValidate('duplicated option -i', 'ValidateDublicatedOptionsError');
-            CLIoptions['-i'] = process.argv[ind + 1];
-
-        }
-
-        if (element === '-o' || element === '--output') {
-
-            if (CLIoptions.hasOwnProperty('-o')) throw new CustomExceptionValidate('duplicated option -o', 'ValidateDublicatedOptionsError');
-            CLIoptions['-o'] = process.argv[ind + 1];
-        }
-
-    });
-
-    log(' CLIoptions = ', CLIoptions);
-} catch (e) {
-    log(e);
-    if (e instanceof CustomExceptionValidate) {
-        process.stderr.write(`${e.name}: ${e.message}`);
-        process.exit(666);
     }
+}
+const run = async () => {
+    try {
 
+        log(process.argv)
+        const CLIoptions = {};
+        let ind = 0;
+        for (const element of process.argv) {
+
+            if (element === '-c' || element === '--config') {
+
+                if (CLIoptions.hasOwnProperty('-c')) throw new CustomExceptionValidate('duplicated option -c', 'ValidateDublicatedOptionsError');
+                CLIoptions['-c'] = process.argv[ind + 1];
+
+            }
+
+            if (element === '-i' || element === '--input') {
+
+                if (CLIoptions.hasOwnProperty('-i')) throw new CustomExceptionValidate('duplicated option -i', 'ValidateDublicatedOptionsError');
+                try {
+                    await fsAccess(process.argv[ind + 1], fs.constants.R_OK);
+                } catch (e) {
+                    throw new CustomExceptionExistsFile(`file is not found or invalid path `, process.argv[ind + 1]);
+                }
+                CLIoptions['-i'] = process.argv[ind + 1];
+
+            }
+
+            if (element === '-o' || element === '--output') {
+
+                if (CLIoptions.hasOwnProperty('-o')) throw new CustomExceptionValidate('duplicated option -o', 'ValidateDublicatedOptionsError');
+                try {
+                    await fsAccess(process.argv[ind + 1], fs.constants.W_OK);
+                } catch (e) {
+                    throw new CustomExceptionExistsFile(`file is not found or invalid path`, process.argv[ind + 1]);
+                }
+                CLIoptions['-o'] = process.argv[ind + 1];
+            }
+
+            ind += 1;
+
+        };
+
+        log(' CLIoptions = ', CLIoptions);
+    } catch (e) {
+        log(e);
+        if (e instanceof CustomExceptionValidate) {
+            process.stderr.write(`${e.name}: ${e.message}`);
+            process.exit(666);
+        }
+        if (e instanceof CustomExceptionExistsFile) {
+            process.stderr.write(`${e.name}: ${e.message}`);
+            process.stderr.write(`${e.path}`);
+            process.exit(666);
+        }
+
+    }
 }
 
+run();
--- test.js
+++ test.js
@@ -1,9 +1,21 @@
-let john = { name: "John" };
+import fs from 'fs';
+import path from 'path';
+import util from 'util';
+const fsAccess = util.promisify(fs.access);
 
-let map = new Map();
-map.set(john, "...");
+async function f() {
+    try {
 
-console.log(map.get({name:'John'}))
-john = null; // перезаписываем ссылку на объект
+        let r = await fsAccess('./input.txt', fs.constants.R_OK);
+        console.log(r)
+    } catch (e) {
+        console.log(e)
+    }
+}
+// console.log(fs.existsSync('E:\_NONDE\_PERN_SHOP\server\.env'))
+// console.log(path.resolve('E:\_NONDE\_PERN_SHOP\server\.env'))
+// fs.access('E:/_NONDE/_PERN_SHOP/server/.env1', fs.constants.W_OK, err => {
+//     console.log(err);
+// });
 
-console.log(map)
+f()

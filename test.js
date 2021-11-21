f2d02d53fd7035800cfa4587ac7d103b55b45329
roboticstechnology <roboticstechnology@gmail.com>
2021-11-21 15:02:25 +0300

    test: add test dunction for chipiera1

--- test.js
+++ test.js
@@ -7,9 +7,13 @@ describe('Test Chipiers', () => {
 
     describe('test chipier Atbash', () => {
         log('test chipier Atbash');
-        test('test char upperCase', () => {
-            log('test char upperCase');
+        test('test EN charS in upperCase', () => {
+            log('test EN chars in upperCase');
             expect(chipierAtbash('ABC')).toEqual('ZYX');
+        });
+        test('test RU charS in upperCase', () => {
+            log('test RU chars in upperCase');
+            expect(chipierAtbash('АБВ')).toEqual('АБВ');
         });
     });
 })
